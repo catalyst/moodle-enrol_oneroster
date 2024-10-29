20609823a07add0c405ec5800c064d2105d46e93
CosmicTG <gustavo.almamorim123@gmail.com>
2024-10-29 21:25:06 +1000

    fixed mustache

--- tests/process_csv_file_check_test.php
+++ tests/process_csv_file_check_test.php
@@ -17,6 +17,7 @@ namespace enrol_oneroster;
 
 use enrol_oneroster\csv_client_helper;
 use enrol_oneroster\csv_client_const_helper;
+use enrol_oneroster\csv_test_helper;
 
 /**
  * One Roster tests for the client_helper class.
@@ -45,132 +46,13 @@ class process_csv_file_check_test extends \advanced_testcase {
      * Set up the test environment.
      */
     protected function setUp(): void {
+        parent::setUp();
+
+        // Create a temporary directory for the test.
         $this->testdir = make_temp_directory('csvtest_dir');
-        $this->manifestpath = $this->testdir . DIRECTORY_SEPARATOR . 'manifest.csv';
 
-        // Creating manifest.csv.
-        $manifestcontent = [
-            ['propertyName', 'value'],
-            ['file.academicSessions', 'bulk'],
-            ['file.classes', 'bulk'],
-            ['file.enrollments', 'bulk'],
-            ['file.orgs', 'bulk'],
-            ['file.users', 'bulk'],
-        ];
-        $handle = fopen($this->manifestpath, 'w');
-        foreach ($manifestcontent as $line) {
-            fputcsv($handle, $line);
-        }
-        fclose($handle);
-
-        // Creating academicSessions.csv.
-        $academicsessionscontent = [
-            csv_client_const_helper::HEADER_ACADEMIC_SESSIONS,
-            [
-                'as-trm-222-1234', 'active', '2023-05-01T18:25:43.511Z', 'Session Title',
-                'term', '2022-09-01', '2022-12-24', 'as-syr-222-2023', '2023'
-            ],
-            [
-                'as-grp-222-2345', '', '', 'Session Title', 'gradingPeriod',
-                '2022-10-02', '2022-12-24', 'as-trm-222-1234', '2023'
-            ]
-        ];
-        $handle = fopen($this->testdir . DIRECTORY_SEPARATOR . 'academicSessions.csv', 'w');
-        foreach ($academicsessionscontent as $line) {
-            fputcsv($handle, $line);
-        }
-        fclose($handle);
-
-        // Creating classes.csv.
-        $classescontent = [
-            csv_client_const_helper::HEADER_CLASSES,
-            [
-                'cls-222-123456', 'active', '2023-05-01T18:25:43.511Z', 'Introduction to Physics',
-                '09,10,11', 'crs-222-2023-456-12345', 'Phys 100 - 1', 'Scheduled',
-                'Room 2-B', 'org-sch-222-456', 'as-trm-222-1234',
-                'Science, Physics, Biology', 'PHY123, ASV120', '1'
-            ],
-            [
-                'cls-222-123478', 'tobedeleted', '2023-05-01T18:25:43.511Z', 'History - 2',
-                '10', 'crs-222-2023-456-23456', '2', 'Scheduled',
-                'Room 18-C', 'org-sch-222-456', 'as-trm-222-1234',
-                'History', 'HIS123', '1,2,3'
-            ]
-        ];
-        $handle = fopen($this->testdir . DIRECTORY_SEPARATOR . 'classes.csv', 'w');
-        foreach ($classescontent as $line) {
-            fputcsv($handle, $line);
-        }
-        fclose($handle);
-
-        // Creating enrollments.csv.
-        $enrollmentscontent = [
-            csv_client_const_helper::HEADER_ENROLLMENTS,
-            [
-                'enr-t-222-12345-123456', 'active', '2023-05-01T18:25:43.511Z', 'cls-222-12345',
-                'org-sch-222-456', 'usr-222-123456', 'teacher', 'FALSE',
-                '2022-03-15', '2022-06-15'
-            ],
-            [
-                'enr-s-222-12345-987654', '', '', 'cls-222-12345', 'org-sch-222-456',
-                'usr-222-987654', 'student', 'FALSE', '2022-03-16', '2022-06-16'
-            ]
-        ];
-        $handle = fopen($this->testdir . DIRECTORY_SEPARATOR . 'enrollments.csv', 'w');
-        foreach ($enrollmentscontent as $line) {
-            fputcsv($handle, $line);
-        }
-        fclose($handle);
-
-        // Creating orgs.csv.
-        $orgscontent = [
-            csv_client_const_helper::HEADER_ORGS,
-            [
-                'org-sch-222-3456', 'active', '2023-05-01T18:25:43.511Z', 'Upper School',
-                'school', 'US', 'org-dpt-222-456'
-            ],
-            [
-                'org-sch-222-456', '', '', 'History Department', 'department',
-                'History', 'org-sch-222-3456'
-            ],
-            [
-                'org-sch-222-7654', 'tobedeleted', '2023-05-01T18:25:43.511Z', 'US History',
-                'department', 'US History', 'org-sch-222-3456'
-            ]
-        ];
-        $handle = fopen($this->testdir . DIRECTORY_SEPARATOR . 'orgs.csv', 'w');
-        foreach ($orgscontent as $line) {
-            fputcsv($handle, $line);
-        }
-        fclose($handle);
-
-        // Creating users.csv.
-        $userscontent = [
-            csv_client_const_helper::HEADER_USERS,
-            [
-                'usr-222-123456', 'active', '2023-05-01', 'TRUE', 'org-sch-222-456',
-                'teacher', 'john.doe', '', 'John', 'Doe', 'Michael', '123456',
-                'john.doe@myschool.com', '6037778888', '6032221111', 'usr-222-66778900',
-                '11', 'Password1*'
-            ],
-            [
-                'usr-222-66778899', '', '', 'TRUE', 'org-sch-222-456',
-                'student', 'mary.jones', '{LDAP:12}', 'Mary', 'Jones', 'Jane',
-                '66778899', 'mary.jones@myschool.com', '6031234567', '6031234567',
-                'usr-222-66778900', '12', 'Password1*'
-            ],
-            [
-                'usr-222-66778900', 'active', '2023-05-01', 'TRUE', 'org-sch-222-456',
-                'parent', 'thomas.joness', '{LDAP:12},{LTI:15},{Fed:23}', 'Thomas',
-                'Jones', 'Paul', '66778900', 'thomas.jones@testemail.com',
-                '6039876543', '6039876543', 'usr-222-66778899', '10', 'Password1*'
-            ]
-        ];
-        $handle = fopen($this->testdir . DIRECTORY_SEPARATOR . 'users.csv', 'w');
-        foreach ($userscontent as $line) {
-            fputcsv($handle, $line);
-        }
-        fclose($handle);
+        // Use the helper class to set up CSV files.
+        csv_test_helper::setUpCsvFiles($this->testdir);
     }
 
     /**
