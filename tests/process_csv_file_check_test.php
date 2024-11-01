fdcd71a77b9e972aab53193be83a67efe146f24c
Ruben <ruben.cooper03@gmail.com>
2024-11-01 14:38:15 +1000

    resolved MOODLE_INTERNAL check

--- tests/process_csv_file_check_test.php
+++ tests/process_csv_file_check_test.php
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 namespace enrol_oneroster;
+
+defined('MOODLE_INTERNAL') || die();
 require(__DIR__ . '/csv_test_helper.php');
 
 use enrol_oneroster\local\csv_client_helper;
