f2c7367314971570fd0c29655c4571ac93d7ca4a
n11558385Harry <n11558385@qut.edu.au>
2025-08-01 15:34:34 +1000

    Added new constants for csv validation.

--- classes/local/v1p2/csv_client_const_helper.php
+++ classes/local/v1p2/csv_client_const_helper.php
@@ -13,14 +13,14 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-namespace enrol_oneroster\local;
+namespace enrol_oneroster\local\v1p2;
 
 use enrol_oneroster\local\v1p1\csv_client_const_helper as csv_client_const_helper_version_one;
 
 /**
  * Class csv_client_const_helper.
  *
- * This class contains constants that are used throughout the OneRoster CSV client.
+ * This class contains constants that are used throughout the OneRoster 1.2 CSV client.
  *
  * @package    enrol_oneroster
  * @copyright  QUT Capstone Team - Abhinav Gandham, Harrison Dyba, Jonathon Foo, Kushi Patel
@@ -28,4 +28,138 @@ use enrol_oneroster\local\v1p1\csv_client_const_helper as csv_client_const_helpe
  */
 class csv_client_const_helper extends csv_client_const_helper_version_one{
    // Insert new header constands here.
+   /**
+     * Header field for the user sourced ID.
+     */
+    const HEADER_USERPROFILE_SOURCEDID = 'userProfileSourcedId';
+
+    /**
+     * Header field for the org sourced ID.
+     */
+    const HEADER_ORG_SOURCEDID = 'orgSourcedId';
+
+    /**
+     * Header field for the role type.
+     */
+    const HEADER_ROLE_TYPE = 'roleType';
+
+    /**
+     * File name for roles.
+     */
+    const FILE_ROLES = 'roles.csv';
+
+    /**
+     * Datatype constant for an role type enumeration.
+     */
+    const DATATYPE_ENUM_ROLE_TYPE = 'enum_role_type';
+
+    /**
+     * Datatype constant for an legacy role type enumeration.
+     */
+    const DATATYPE_ENUM_ROLE_ROLE = 'enum_role';
+
+    /**
+     * Valid roles for enrollments.
+     *
+     * @var array
+     */
+    const VALID_ENROLLMENT_ROLES = ['administrator', 'proctor', 'student', 'teacher'];
+     /**
+     * Valid roles for Role.
+     *
+     * @var array
+     */
+    const VALID_ROLE_ROLES = ['aide' , 'counselor' , 'districtAdministrator' , 'guardian' ,
+                            'parent' , 'principal' , 'proctor' , 'relative' , 'siteAdministrator'
+                            , 'student' , 'systemAdministrator' , 'teacher'];
+
+    /**
+     * Valid role types.
+     *
+     * @var array
+     */
+    const VALID_ROLE_TYPE =[ 'primary', 'secondary'];
+
+     /**
+   * Id field for the vendor id
+   */
+   const HEADER_VENDOR_ID = 'vendorId';
+
+   /**
+    * Id field for the application id
+    */
+   const HEADER_APPLICATION_ID = 'applicationId';
+
+   /**
+    * Type field for the user profile
+    */
+   const HEADER_PROFILE_TYPE = 'profileType';
+
+   /**
+    * Credential type field for the credential type
+    */
+   const HEADER_CREDENTIAL_TYPE = 'credentialType';
+
+   /**
+    * The description field for the user profile
+    */
+   const HEADER_DESCRIPTION = 'description';
+
+   /**
+    * File name for the user profiles
+    */
+   const FILE_USERPROFILES = 'userprofiles.csv';
+
+
+    /**
+     * Header fields for the roles file.
+     *
+     * @var array
+     */
+    const HEADER_ROLES = [
+        self::HEADER_SOURCEDID,
+        self::HEADER_STATUS,
+        self::HEADER_DATE_LAST_MODIFIED,
+        self::HEADER_USER_SOURCEDID,
+        self::HEADER_ROLE_TYPE,
+        self::HEADER_ROLE,
+        self::HEADER_BEGIN_DATE,
+        self::HEADER_END_DATE,
+        self::HEADER_ORG_SOURCEDID,
+        self::HEADER_USERPROFILE_SOURCEDID
+    ];
+
+    /**
+     * Header fields for user profiles
+     *
+     *
+     * @var array
+     */
+    const HEADER_USERPROFILES = [
+        self::HEADER_SOURCEDID,
+        self::HEADER_STATUS,
+        self::HEADER_DATE_LAST_MODIFIED,
+        self::HEADER_USER_SOURCEDID,
+        self::HEADER_PROFILE_TYPE,
+        self::HEADER_VENDOR_ID,
+        self::HEADER_APPLICATION_ID,
+        self::HEADER_DESCRIPTION,
+        self::HEADER_CREDENTIAL_TYPE,
+        self::HEADER_USERNAME,
+        self::HEADER_PASSWORD
+     ];
+
+    /**
+     * Required files and their corresponding headers.
+     *
+     * @var array
+     */
+    const REQUIRED_FILES = [
+        self::FILE_ACADEMIC_SESSIONS => self::HEADER_ACADEMIC_SESSIONS,
+        self::FILE_CLASSES => self::HEADER_CLASSES,
+        self::FILE_ENROLLMENTS => self::HEADER_ENROLLMENTS,
+        self::FILE_ORGS => self::HEADER_ORGS,
+        self::FILE_USERS => self::HEADER_USERS,
+        self::FILE_ROLES => self:: HEADER_ROLES,
+    ];
 }
--- classes/local/v1p2/csv_client_helper.php
+++ classes/local/v1p2/csv_client_helper.php
@@ -13,10 +13,12 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-namespace enrol_oneroster\local\v1p1;
+namespace enrol_oneroster\local\v1p2;
 
-use enrol_oneroster\local\v1p2\csv_client_const_helper as csv_client_const_helper;
+use enrol_oneroster\local\csv_client_const_helper;
 use enrol_oneroster\local\v1p1\csv_client_helper as csv_client_helper_version_one;
+
+use function PHPUnit\Framework\assertEquals;
 
 /**
  * Class csv_client_helper
@@ -28,5 +30,207 @@ use enrol_oneroster\local\v1p1\csv_client_helper as csv_client_helper_version_on
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class csv_client_helper extends csv_client_helper_version_one{
-   // Insert new logic here.
-}
+   /**
+     * Get the expected data types for each file.
+     *
+     * @return array An array containing the expected data types for each file.
+     */
+    public static function get_file_datatypes(): array {
+      //call parent
+      $data = parent::get_file_datatypes();
+      //add roles
+      $roles=
+         [
+            csv_client_const_helper::FILE_ROLES => [
+                csv_client_const_helper::HEADER_SOURCEDID => csv_client_const_helper::DATATYPE_GUID,
+                csv_client_const_helper::HEADER_STATUS => [
+                    csv_client_const_helper::DATATYPE_ENUM_STATUS,
+                    csv_client_const_helper::DATATYPE_NULL
+                ],
+                csv_client_const_helper::HEADER_DATE_LAST_MODIFIED => [
+                    csv_client_const_helper::DATATYPE_DATETIME,
+                    csv_client_const_helper::DATATYPE_NULL
+                ],
+                csv_client_const_helper::HEADER_USER_SOURCEDID => csv_client_const_helper::DATATYPE_GUID,
+                csv_client_const_helper::HEADER_BEGIN_DATE => [
+                    csv_client_const_helper::DATATYPE_DATE,
+                    csv_client_const_helper::DATATYPE_NULL
+                ],
+                csv_client_const_helper::HEADER_END_DATE => [
+                    csv_client_const_helper::DATATYPE_DATE,
+                    csv_client_const_helper::DATATYPE_NULL
+                ],
+                csv_client_const_helper::HEADER_ORG_SOURCEDID => csv_client_const_helper::DATATYPE_GUID,
+                csv_client_const_helper::HEADER_USERPROFILE_SOURCEDID => csv_client_const_helper::DATATYPE_GUID
+            ]
+         ];
+      $mergedarray = array_merge($data, $roles);
+      return $mergedarray;
+      }
+
+    /**
+     * Get the validator functions for each data type.
+     *
+     * @return array An array containing the validator functions for each data type.
+     */
+    public static function get_validator(): array {
+        return [
+            csv_client_const_helper::DATATYPE_GUID => 'is_guid_type',
+            csv_client_const_helper::DATATYPE_INT => 'is_int_type',
+            csv_client_const_helper::DATATYPE_DATETIME => 'is_datetime_type',
+            csv_client_const_helper::DATATYPE_DATE => 'is_date_type',
+            csv_client_const_helper::DATATYPE_YEAR => 'is_year_type',
+            csv_client_const_helper::DATATYPE_ENUM_STATUS => 'is_status_enum_type',
+            csv_client_const_helper::DATATYPE_ENUM_TYPE => 'is_type_enum',
+            csv_client_const_helper::DATATYPE_ARRAY_GUID => 'is_valid_guid_list',
+            csv_client_const_helper::DATATYPE_ARRAY_GRADE => 'is_valid_grades',
+            csv_client_const_helper::DATATYPE_GRADE => 'is_valid_grade',
+            csv_client_const_helper::DATATYPE_STRING_EMAIL => 'is_email_type',
+            csv_client_const_helper::DATATYPE_ARRAY_USERIDS => 'is_valid_user_id',
+            csv_client_const_helper::DATATYPE_ENUM_ROLE_USER => 'is_role_user_enum',
+            csv_client_const_helper::DATATYPE_ENUM_TYPE_ENROL => 'is_enrollment_role_enum',
+            csv_client_const_helper::DATATYPE_ENUM_PRIMARY => 'is_primary_enum',
+            csv_client_const_helper::DATATYPE_ENUM_CLASS_TYPE => 'is_class_type_enum',
+            csv_client_const_helper::DATATYPE_ENUM_ORG_TYPE => 'is_org_type_enum',
+            csv_client_const_helper::DATATYPE_ENUM_ROLE_TYPE => 'is_role_type_enum',
+            csv_client_const_helper::DATATYPE_ENUM_ROLE_ROLE => 'is_role_enum',
+            csv_client_const_helper::DATATYPE_ARRAY_SUBJECTS => 'is_list_of_strings',
+            csv_client_const_helper::DATATYPE_ARRAY_SUBJECT_CODES => 'is_valid_subject_codes',
+            csv_client_const_helper::DATATYPE_PASSWORD => 'is_valid_password',
+            csv_client_const_helper::DATATYPE_ARRAY_PERIODS => 'is_valid_periods',
+            csv_client_const_helper::DATATYPE_STRING => 'is_valid_human_readable_string',
+        ];
+    }
+    /**
+     * Function to display missing and invalid files.
+     *
+     * @param array $missingfiles An array containing the missing files and invalid headers.
+     * @return string The error messages to be displayed.
+     */
+    public static function display_missing_and_invalid_files(array $missingfiles): string {
+        $criticalfiles = [
+            csv_client_const_helper::FILE_ACADEMIC_SESSIONS,
+            csv_client_const_helper::FILE_CLASSES,
+            csv_client_const_helper::FILE_ENROLLMENTS,
+            csv_client_const_helper::FILE_ORGS,
+            csv_client_const_helper::FILE_USERS,
+            csv_client_const_helper::FILE_ROLES
+        ];
+
+        $errormessage = '';
+
+        if (!empty($missingfiles['missingfiles'])) {
+            $criticalmissingfiles = [];
+            $noncriticalfiles = [];
+
+            foreach ($missingfiles['missingfiles'] as $missingfile) {
+                if (in_array($missingfile, $criticalfiles, true)) {
+                    $criticalmissingfiles[] = $missingfile;
+                } else {
+                    $noncriticalfiles[] = $missingfile;
+                }
+            }
+
+            if (!empty($criticalmissingfiles)) {
+                $criticalfileslist = implode(', ', $criticalmissingfiles);
+                $errormessage .= get_string(
+                    'missingfiles',
+                    'enrol_oneroster',
+                    (object)['files' => $criticalfileslist]
+                ) . '<br>';
+            }
+
+            foreach ($noncriticalfiles as $noncriticalfile) {
+                $errormessage .= get_string(
+                    'invalid_manifest_selection',
+                    'enrol_oneroster',
+                    (object)['manifest' => $noncriticalfile]
+                ) . '<br>';
+            }
+        }
+
+        if (!empty($missingfiles['invalidheaders'])) {
+            $invalidheaderslist = implode(', ', $missingfiles['invalidheaders']);
+            $errormessage .= get_string(
+                'invalidheaders',
+                'enrol_oneroster',
+                (object)['headers' => $invalidheaderslist]
+            ) . '<br>';
+        }
+
+        return $errormessage;
+    }
+
+    /**
+     * Function to get the header for a given file.
+     *
+     * @param string $filename The name of the file.
+     * @return array The header for the given file.
+     */
+    public static function get_header(string $filename): array {
+
+      $parentreturn = parent::get_header($filename);
+
+      if ($parentreturn != []) {
+         return $parentreturn;
+      }
+
+      switch ($filename) {
+            case csv_client_const_helper::FILE_ROLES:
+                return csv_client_const_helper::HEADER_ROLES;
+            default:
+                return [];
+      }
+    }
+
+    /**
+     * Check if a value is of type role enum.
+     *
+     * @param string $value The value to check.
+     * @return bool True if the value is of type role enum, false otherwise.
+     */
+    public static function is_enrollment_role_enum(string $value): bool {
+        return in_array(strtolower($value), csv_client_const_helper::VALID_ENROLLMENT_ROLES, true);
+    }
+
+    /**
+     * Check if a value is of type role type enum.
+     *
+     * @param mixed $value The value to check.
+     * @return bool True if the value is of type role type enum, false otherwise.
+     */
+    public static function is_role_type_enum($value): bool {
+        return in_array(strtolower($value), csv_client_const_helper::VALID_ROLE_TYPE, true);
+    }
+    /**
+     * Check if a value is of type role enum.
+     *
+     * @param mixed $value The value to check.
+     * @return bool True if the value is of type role enum, false otherwise.
+     */
+    public static function is_role_enum($value): bool {
+        return in_array(strtolower($value), csv_client_const_helper::VALID_ROLE_ROLES, true);
+    }
+
+    /**
+     * Check if a value is of type org type enum.
+     *
+     * @param mixed $value The value to check.
+     * @return bool True if the value is of type org type enum, false otherwise.
+     */
+    public static function is_org_type_enum($value): bool {
+        return in_array(strtolower($value), csv_client_const_helper::VALID_ORG_TYPES, true);
+    }
+
+
+
+    /**
+     * Check if a value is of type role user enum.
+     *
+     * @param mixed $value The value to check.
+     * @return bool True if the value is of type role user enum, false otherwise.
+     */
+    public static function is_role_user_enum($value): bool {
+        return in_array(strtolower($value), csv_client_const_helper::VALID_ROLES_USERS, true);
+    }
+   }
