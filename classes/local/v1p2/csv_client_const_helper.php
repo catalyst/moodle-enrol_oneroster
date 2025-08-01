396eaf480169c51727838533c0084e135976e662
n11558385Harry <n11558385@qut.edu.au>
2025-08-01 17:05:05 +1000

    roles added to client helper and const

--- classes/local/v1p2/csv_client_const_helper.php
+++ classes/local/v1p2/csv_client_const_helper.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-namespace enrol_oneroster\local;
+namespace enrol_oneroster\local\v1p2;
 
 use enrol_oneroster\local\v1p1\csv_client_const_helper as csv_client_const_helper_version_one;
 
