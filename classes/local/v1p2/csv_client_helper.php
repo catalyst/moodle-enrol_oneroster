e003bc7823c340bb0d6993987021da0051b762fa
abhinavgandham <abhinavgandham@gmail.com>
2025-08-05 14:41:29 +1000

    Improved code.

--- classes/local/v1p2/csv_client_helper.php
+++ classes/local/v1p2/csv_client_helper.php
@@ -34,27 +34,32 @@ class csv_client_helper extends csv_client_helper_version_one{
      * @return array An array containing the expected data types for each file.
      */
    public static function get_file_datatypes(): array {
-      return [
-         csv_client_const_helper::FILE_USERPROFILES => [
-            csv_client_const_helper::HEADER_SOURCEDID => csv_client_const_helper::DATATYPE_GUID,
-            csv_client_const_helper::HEADER_STATUS => [
-                csv_client_const_helper::DATATYPE_ENUM_STATUS,
-                csv_client_const_helper::DATATYPE_NULL
-            ],
-            csv_client_const_helper::HEADER_DATE_LAST_MODIFIED => [
-                csv_client_const_helper::DATATYPE_DATETIME,
-                csv_client_const_helper::DATATYPE_NULL
-            ],
-            csv_client_const_helper::HEADER_USER_SOURCEDID => csv_client_const_helper::DATATYPE_GUID,
-            csv_client_const_helper::HEADER_PROFILE_TYPE => csv_client_const_helper::DATATYPE_STRING,
-            csv_client_const_helper::HEADER_VENDOR_ID => csv_client_const_helper::DATATYPE_STRING,
-            csv_client_const_helper::HEADER_APPLICATION_ID => csv_client_const_helper::DATATYPE_STRING,
-            csv_client_const_helper::HEADER_DESCRIPTION => csv_client_const_helper::DATATYPE_STRING,
-            csv_client_const_helper::HEADER_CREDENTIAL_TYPE => csv_client_const_helper::DATATYPE_STRING,
-            csv_client_const_helper::HEADER_USERNAME => csv_client_const_helper::DATATYPE_STRING,
-            csv_client_const_helper::HEADER_PASSWORD => csv_client_const_helper::DATATYPE_STRING
-         ]
-      ];
+    // Gets the already existing datatypes from the parent class.
+    $parentdatatypes = parent::get_file_datatypes();
+    // The new userprofile data that will be added to the datatypes.
+    $userprofiledata = [
+        csv_client_const_helper::FILE_USERPROFILES => [
+           csv_client_const_helper::HEADER_SOURCEDID => csv_client_const_helper::DATATYPE_GUID,
+           csv_client_const_helper::HEADER_STATUS => [
+               csv_client_const_helper::DATATYPE_ENUM_STATUS,
+               csv_client_const_helper::DATATYPE_NULL
+           ],
+           csv_client_const_helper::HEADER_DATE_LAST_MODIFIED => [
+               csv_client_const_helper::DATATYPE_DATETIME,
+               csv_client_const_helper::DATATYPE_NULL
+           ],
+           csv_client_const_helper::HEADER_USER_SOURCEDID => csv_client_const_helper::DATATYPE_GUID,
+           csv_client_const_helper::HEADER_PROFILE_TYPE => csv_client_const_helper::DATATYPE_STRING,
+           csv_client_const_helper::HEADER_VENDOR_ID => csv_client_const_helper::DATATYPE_STRING,
+           csv_client_const_helper::HEADER_APPLICATION_ID => csv_client_const_helper::DATATYPE_STRING,
+           csv_client_const_helper::HEADER_DESCRIPTION => csv_client_const_helper::DATATYPE_STRING,
+           csv_client_const_helper::HEADER_CREDENTIAL_TYPE => csv_client_const_helper::DATATYPE_STRING,
+           csv_client_const_helper::HEADER_USERNAME => csv_client_const_helper::DATATYPE_STRING,
+           csv_client_const_helper::HEADER_PASSWORD => csv_client_const_helper::DATATYPE_STRING
+        ]
+     ];
+     // Merges the parent datatypes with the new userprofile datatypes.
+     return array_merge($parentdatatypes, $userprofiledata);
    }
 
     /**
@@ -124,17 +129,13 @@ class csv_client_helper extends csv_client_helper_version_one{
      * @return array The header for the given file.
      */
     public static function get_header(string $filename): array {
+      $parentReturn = parent::get_header($filename);
+
+      if ($parentReturn !== []) {
+        return $parentReturn;
+      };
+
       switch ($filename) {
-          case csv_client_const_helper::FILE_ACADEMIC_SESSIONS:
-              return csv_client_const_helper::HEADER_ACADEMIC_SESSIONS;
-          case csv_client_const_helper::FILE_CLASSES:
-              return csv_client_const_helper::HEADER_CLASSES;
-          case csv_client_const_helper::FILE_ENROLLMENTS:
-              return csv_client_const_helper::HEADER_ENROLLMENTS;
-          case csv_client_const_helper::FILE_ORGS:
-              return csv_client_const_helper::HEADER_ORGS;
-          case csv_client_const_helper::FILE_USERS:
-              return csv_client_const_helper::HEADER_USERS;
           case csv_client_const_helper::FILE_USERPROFILES:
               return csv_client_const_helper::HEADER_USERPROFILES;
           default:
