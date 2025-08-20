91dc1995bb0c51504db5319c8f1f5a265e23a9d7
Kpfp2711 <khushipatel27kk@gmail.com>
2025-08-20 12:38:46 +1000

    Add imsx_statusinfo implementation with enums and unit test

---
+++ enrol/oneroster/classes/local/oneroster_testcase.php
@@ -0,0 +1,12 @@
+<?php
+// @copyright Khushi
+
+namespace enrol_oneroster\local;
+
+defined('MOODLE_INTERNAL') || die();
+
+use advanced_testcase;
+
+class oneroster_testcase extends advanced_testcase {
+    // You can add common setup here later.
+}
