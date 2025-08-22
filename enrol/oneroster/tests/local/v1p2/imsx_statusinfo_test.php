25a11c123d2c3033bc950945418a3dad7fe154fd
Kpfp2711 <khushipatel27kk@gmail.com>
2025-08-22 15:44:04 +1000

    Add OneRoster 1.2 statusInfo implementation and tests

---
+++ enrol/oneroster/tests/local/v1p2/imsx_statusinfo_test.php
@@ -0,0 +1,99 @@
+<?php
+namespace enrol_oneroster\tests\local\v1p2;
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * One Roster Enrolment Client.
+ *
+ * @package    enrol_oneroster
+ * @copyright  Andrew Nicols <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+/**
+ * One Roster 1.1 Factory Manager.
+ *
+ * @package    enrol_oneroster
+ * @copyright  QUT Capstone Team - Abhinav Gandham, Harrison Dyba, Jonathon Foo, Khushi Patel
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use enrol_oneroster\local\v1p2\imsx_statusinfo;
+use enrol_oneroster\local\v1p2\codeMajor;
+use enrol_oneroster\local\v1p2\severity;
+use enrol_oneroster\local\v1p2\minorFieldValue;
+
+// Explicit requires to avoid namespace autoload issues
+require_once(__DIR__ . '/../../../../local/v1p2/codeMajor.php');
+require_once(__DIR__ . '/../../../../local/v1p2/severity.php');
+require_once(__DIR__ . '/../../../../local/v1p2/minorFieldValue.php');
+require_once(__DIR__ . '/../../../../local/v1p2/codeMinor.php');
+require_once(__DIR__ . '/../../../../local/v1p2/imsx_statusinfo.php');
+
+final class imsx_statusinfo_test extends \enrol_oneroster\local\oneroster_testcase {
+
+
+    public function test_construct_success() {
+        $data = (object)[
+            'codeMajor' => 'success',
+            'codeMinor' => (object)[
+                'fieldName' => 'filter',
+                'fieldValue' => 'fullsuccess',
+            ],
+            'description' => 'Request completed successfully',
+            'severity' => 'status',
+        ];
+
+        $status = new imsx_statusinfo($data);
+
+        $this->assertInstanceOf(imsx_statusinfo::class, $status);
+        $this->assertEquals(codeMajor::success, $status->codeMajor);
+        $this->assertEquals('Request completed successfully', $status->description);
+        $this->assertEquals(severity::status, $status->severity);
+        $this->assertEquals('filter', $status->codeMinor->fieldName);
+        $this->assertEquals(minorFieldValue::fullsuccess, $status->codeMinor->codeMinorFieldValue);
+    }
+
+    public function test_missing_codeMajor_throws_exception() {
+        $this->expectException(\moodle_exception::class);
+
+        $data = (object)[
+            'codeMinor' => (object)[
+                'fieldName' => 'filter',
+                'fieldValue' => 'fullsuccess',
+            ],
+            'description' => 'Missing codeMajor field',
+            'severity' => 'status',
+        ];
+
+        new imsx_statusinfo($data);
+    }
+
+    public function test_invalid_codeMajor_throws_exception() {
+        $this->expectException(\moodle_exception::class);
+
+        $data = (object)[
+            'codeMajor' => 'invalid_value',
+            'codeMinor' => (object)[
+                'fieldName' => 'filter',
+                'fieldValue' => 'fullsuccess',
+            ],
+            'description' => 'Invalid codeMajor test',
+            'severity' => 'status',
+        ];
+
+        new imsx_statusinfo($data);
+    }
+}
